29ca1b5c99f48b2d7453e68cb191c0a8ac069c61
Kanchanok <kanchanok1417@gmail.com>
2026-01-04 16:13:29 +0700

    Update

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
+int fibonacci(int a);
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+int fibonacci(int a){
+    int f[a] = {0,1};
+    int i = 2;
+    if (a == 0 || a == 1) return a;
+    while (i<= a){
+        f[i] = f[i-1]+f[i-2];
+        i++;
+    }
+    return f[a];
 }
